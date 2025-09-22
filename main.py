8f667b33212d99cd9cb4f6b258c4af570889ac06
PokeMasterCP <christian.adrn.perez@gmail.com>
2025-09-22 11:49:22 -0500

    Added 60FPS game clock

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -13,6 +15,7 @@ def main():
                 return
         screen.fill(color="black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
